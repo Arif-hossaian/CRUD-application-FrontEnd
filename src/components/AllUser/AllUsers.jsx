3f8234bc64ad46fabe4ab32eb1ff6e6053562f76
Arif-hossaian <arifhossain42136@gmail.com>
2021-07-14 13:12:45 +0600

    initial setup and navbar created

---
+++ src/components/AllUser/AllUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllUsers = () => {
+    return (
+        <div>
+           allUsers 
+        </div>
+    )
+}
+
+export default AllUsers
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as NavBar} from "./Navbar/NavBar.jsx"
+export { default as Home} from "./Home/Home.jsx"
+export { default as AllUsers} from "./AllUser/AllUsers.jsx"
+export { default as AddUsers} from "./AddUser/AddUsers.jsx"
